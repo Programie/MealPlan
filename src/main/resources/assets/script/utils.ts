804880381738cefbed11b25b49af4d3bd465155f
Michael Wieland <git@selfcoders.com>
2023-04-10 15:25:58 +0200

    Implemented edit modals, refactored editor js code

---
+++ src/main/resources/assets/script/utils.ts
@@ -0,0 +1,3 @@
+export function string2boolean(value: string) {
+    return value?.toLowerCase?.() === "true";
+}
--- src/main/resources/assets/script/week-edit.ts
+++ src/main/resources/assets/script/week-edit.ts
@@ -1,44 +1,120 @@
 import "../style/main.scss";
 
-import "bootstrap";
+import {Dropdown, Modal} from "bootstrap";
 import * as Mustache from "mustache";
 
 import MealAutocompletion from "./meal-autocompletion";
+import {string2boolean} from "./utils";
 
-window.onload = () => {
-    let dataChanged = false;
-    let mealAutocompletion = new MealAutocompletion(".week-edit-meal input");
+class Editor {
+    private dataChanged: boolean = false;
+    private autocompletion: MealAutocompletion;
 
-    function addChangeEventListener(element: Element) {
-        element.addEventListener("change", () => {
-            dataChanged = true;
+    constructor() {
+        this.autocompletion = new MealAutocompletion(".week-edit-meal input");
+
+        this.configureModal("url", this.saveUrlModal);
+        this.configureModal("notification", this.saveNotificationModal);
+        this.configureAddButtons();
+
+        document.querySelectorAll(".week-edit-meal").forEach((element) => {
+            this.addMealEventListeners(element);
+        });
+
+        window.addEventListener("beforeunload", (event) => {
+            if (this.dataChanged) {
+                event.preventDefault();
+            }
         });
     }
 
-    document.querySelectorAll(".week-edit-meal input").forEach(addChangeEventListener);
+    addMealEventListeners(containerElement: Element) {
+        let inputElement = containerElement.querySelector("input");
 
-    document.querySelectorAll(".week-edit-meal-add").forEach((element: HTMLButtonElement) => {
-        element.addEventListener("click", () => {
-            let date = element.closest("tr").getAttribute("data-date");
-            let mealType = element.closest("td").getAttribute("data-type");
-
-            let newContainer = Mustache.render(document.querySelector("#week-edit-meal-template").innerHTML, {
-                date: date,
-                type: mealType
-            });
-
-            element.insertAdjacentHTML("beforebegin", newContainer);
-            let newInputElement = element.parentElement.querySelector(".week-edit-meal:last-of-type input");
-
-            mealAutocompletion.updateElement(newInputElement);
-            addChangeEventListener(newInputElement);
+        inputElement.addEventListener("change", () => {
+            this.dataChanged = true;
         });
-    });
 
-    window.addEventListener("beforeunload", (event) => {
-        if (dataChanged) {
-            event.preventDefault();
-        }
-    });
+        containerElement.querySelector(".week-edit-meal-button-link").addEventListener("click", () => {
+            this.showEditUrlModal(inputElement.getAttribute("data-id"), inputElement.getAttribute("data-url"));
+        });
+
+        containerElement.querySelector(".week-edit-meal-button-notification").addEventListener("click", () => {
+            this.showEditNotificationModal(inputElement.getAttribute("data-id"), string2boolean(inputElement.getAttribute("data-notification-enabled")), inputElement.getAttribute("data-notification-time"));
+        });
+    }
+
+    configureAddButtons() {
+        document.querySelectorAll(".week-edit-meal-add").forEach((element: HTMLButtonElement) => {
+            element.addEventListener("click", () => {
+                this.addMeal(element.closest("td").querySelector(".week-edit-meal-container"));
+            });
+        });
+    }
+
+    addMeal(containerElement: Element) {
+        let date = containerElement.closest("tr").getAttribute("data-date");
+        let mealType = containerElement.closest("td").getAttribute("data-type");
+
+        let newContainer = Mustache.render(document.querySelector("#week-edit-meal-template").innerHTML, {
+            date: date,
+            type: mealType
+        });
+
+        containerElement.insertAdjacentHTML("beforeend", newContainer);
+        let newContainerElement = containerElement.parentElement.querySelector(".week-edit-meal:last-of-type");
+
+        this.autocompletion.updateElement(newContainerElement.querySelector("input"));
+        this.addMealEventListeners(newContainerElement);
+    }
+
+    showEditUrlModal(mealId: string, url: string) {
+        let modalElement = document.querySelector("#week-edit-url-modal");
+        modalElement.setAttribute("data-meal-id", mealId);
+
+        let urlInputElement: HTMLInputElement = modalElement.querySelector("#week-edit-url-input");
+        urlInputElement.value = url;
+
+        let modal = new Modal(modalElement);
+        modal.show();
+    }
+
+    saveUrlModal(modalElement: Element, mealInputElement: HTMLInputElement) {
+        let urlInputElement: HTMLInputElement = modalElement.querySelector("#week-edit-url-input");
+
+        mealInputElement.setAttribute("data-url", urlInputElement.value);
+    }
+
+    showEditNotificationModal(mealId: string, enable: boolean, time: string) {
+        let modalElement = document.querySelector("#week-edit-notification-modal");
+        modalElement.setAttribute("data-meal-id", mealId);
+
+        let enableElement: HTMLInputElement = modalElement.querySelector("#week-edit-notification-enable");
+        enableElement.checked = enable;
+
+        let modal = new Modal(modalElement);
+        modal.show();
+    }
+
+    saveNotificationModal(modalElement: Element, mealInputElement: HTMLInputElement) {
+    }
+
+    configureModal(name: string, callback: (modalElement: Element, mealInputElement: HTMLInputElement) => void) {
+        let modalElement = document.querySelector(`#week-edit-${name}-modal`);
+        modalElement.querySelector(".modal-button-ok").addEventListener("click", () => {
+            let mealId = modalElement.getAttribute("data-meal-id");
+
+            let mealInputElement: HTMLInputElement = document.querySelector(`.week-edit-meal input[data-id='${mealId}']`);
+
+            callback(modalElement, mealInputElement);
+
+            this.dataChanged = true;
+
+            Modal.getInstance(modalElement).hide();
+        });
+    }
+}
+
+window.onload = () => {
+    new Editor();
 };
-
