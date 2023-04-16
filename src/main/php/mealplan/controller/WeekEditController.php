95265f6a217bec6bde949c94e87968cb1aa87f56
Michael Wieland <git@selfcoders.com>
2023-04-16 20:15:15 +0200

    Move code to separate methods

--- src/main/php/mealplan/controller/WeekEditController.php
+++ src/main/php/mealplan/controller/WeekEditController.php
@@ -4,6 +4,7 @@ namespace mealplan\controller;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\RequestOptions;
 use mealplan\Config;
 use mealplan\Date;
@@ -52,112 +53,11 @@ class WeekEditController extends AbstractController
 
         $savedWeek = null;
 
-        foreach ($inputData as $inputDataIndex => $mealData) {
-            $id = Sanitize::cleanInt($mealData["id"] ?? null);
-            $type = Sanitize::cleanInt($mealData["type"] ?? null);
-            $date = Sanitize::cleanString($mealData["date"] ?? null);
-            $notificationData = $mealData["notification"] ?? [];
+        foreach ($inputData as $index => $mealData) {
+            $meal = $this->saveItem($mealData, $index, $space, $mealTypeRepository, $mealRepository, $entityManager);
 
-            try {
-                $text = Sanitize::cleanString($mealData["text"] ?? null, 200);
-            } catch (StringTooLongException) {
-                throw new BadRequestHttpException(sprintf("Text of entry %d is too long", $inputDataIndex));
-            }
-
-            try {
-                $url = Sanitize::cleanString($mealData["url"] ?? null, 2048);
-            } catch (StringTooLongException) {
-                throw new BadRequestHttpException(sprintf("URL of entry %d is too long", $inputDataIndex));
-            }
-
-            // No ID and no text -> ignore item
-            if ($id === null and $text === null) {
-                continue;
-            }
-
-            // ID specified but no text -> delete item
-            if ($id !== null and $text === null) {
-                $meal = $mealRepository->find($id);// TODO: Restrict to current space?
-
-                if ($meal !== null) {
-                    $entityManager->remove($meal);
-                }
-
-                continue;
-            }
-
-            if ($date === null) {
-                throw new BadRequestHttpException(sprintf("Missing date in entry %d", $inputDataIndex));
-            }
-
-            $notificationTime = Sanitize::cleanString($notificationData["time"] ?? null);
-
-            try {
-                $notificationText = Sanitize::cleanString($notificationData["text"] ?? null, 200);
-            } catch (StringTooLongException) {
-                throw new BadRequestHttpException(sprintf("Notification text of entry %d is too long", $inputDataIndex));
-            }
-
-            if ($notificationTime === null) {
-                $notificationDateTime = null;
-            } else {
-                try {
-                    $notificationDateTime = new DateTime($notificationTime);
-                } catch (Exception) {
-                    throw new BadRequestHttpException(sprintf("Unable to parse notification time '%s' in entry %d", $notificationTime, $inputDataIndex));
-                }
-            }
-
-            /**
-             * @var $mealType MealType
-             */
-            $mealType = $mealTypeRepository->find($type);// TODO: Restrict to current space?
-            if ($mealType === null) {
-                throw new BadRequestHttpException(sprintf("Invalid meal type %d in entry %d", $type, $inputDataIndex));
-            }
-
-            if ($id === null) {
-                $meal = new Meal;
-                $notification = null;
-            } else {
-                $meal = $mealRepository->find($id);// TODO: Restrict to current space?
-
-                if ($meal === null) {
-                    throw new BadRequestHttpException(sprintf("Meal entry with ID %d does not exist", $id));
-                }
-
-                $notification = $meal->getNotification();
-            }
-
-            try {
-                $meal->setDate(new Date($date));
-            } catch (Exception) {
-                throw new BadRequestHttpException(sprintf("Unable to parse date '%s' in entry %d", $date, $inputDataIndex));
-            }
-
-            $meal->setSpace($space);
-            $meal->setType($mealType);
-            $meal->setText($text);
-            $meal->setUrl($url);
-
-            $entityManager->persist($meal);
-
-            $savedWeek = $meal->getDate()->getStartOfWeek();
-
-            if ($notificationDateTime === null) {
-                if ($notification !== null) {
-                    $entityManager->remove($notification);
-                }
-            } else {
-                if ($notification === null) {
-                    $notification = new Notification;
-                    $notification->setMeal($meal);
-                }
-
-                $notification->setTime($notificationDateTime);
-                $notification->setText($notificationText);
-                $notification->setTriggered(false);
-                $entityManager->persist($notification);
+            if ($meal !== null) {
+                $savedWeek = $meal->getDate()->getStartOfWeek();
             }
         }
 
@@ -165,22 +65,144 @@ class WeekEditController extends AbstractController
         $entityManager->commit();
 
         try {
-            $saveConfig = $config->get("app.save") ?? [];
-            $webhookUrl = $saveConfig["webhook-url"] ?? null;
-            if ($webhookUrl !== null) {
-                $client = new Client;
-                $client->post($webhookUrl, [
-                    RequestOptions::TIMEOUT => $saveConfig["webhook-timeout"] ?? 0,
-                    RequestOptions::JSON => [
-                        "space" => $spaceId,
-                        "week" => $savedWeek?->formatForKey()
-                    ]
-                ]);
-            }
+            $this->triggerSaveWebhook($config, $space, $savedWeek);
         } catch (Throwable $exception) {
             $logger->error($exception);
         }
 
         return $this->json(["status" => "ok"]);
+    }
+
+    private function saveItem($mealData, int $itemIndex, Space $space, MealTypeRepository $mealTypeRepository, MealRepository $mealRepository, EntityManagerInterface $entityManager): ?Meal
+    {
+        $id = Sanitize::cleanInt($mealData["id"] ?? null);
+        $date = Sanitize::cleanString($mealData["date"] ?? null);
+
+        try {
+            $text = Sanitize::cleanString($mealData["text"] ?? null, 200);
+        } catch (StringTooLongException) {
+            throw new BadRequestHttpException(sprintf("Text of entry %d is too long", $itemIndex));
+        }
+
+        try {
+            $url = Sanitize::cleanString($mealData["url"] ?? null, 2048);
+        } catch (StringTooLongException) {
+            throw new BadRequestHttpException(sprintf("URL of entry %d is too long", $itemIndex));
+        }
+
+        // No ID and no text -> ignore item
+        if ($id === null and $text === null) {
+            return null;
+        }
+
+        // ID specified but no text -> delete item
+        if ($id !== null and $text === null) {
+            $meal = $mealRepository->find($id);// TODO: Restrict to current space?
+
+            if ($meal !== null) {
+                $entityManager->remove($meal);
+            }
+
+            return null;
+        }
+
+        if ($date === null) {
+            throw new BadRequestHttpException(sprintf("Missing date in entry %d", $itemIndex));
+        }
+
+        $notificationData = $mealData["notification"] ?? [];
+        $notificationTime = Sanitize::cleanString($notificationData["time"] ?? null);
+
+        try {
+            $notificationText = Sanitize::cleanString($notificationData["text"] ?? null, 200);
+        } catch (StringTooLongException) {
+            throw new BadRequestHttpException(sprintf("Notification text of entry %d is too long", $itemIndex));
+        }
+
+        if ($notificationTime === null) {
+            $notificationDateTime = null;
+        } else {
+            try {
+                $notificationDateTime = new DateTime($notificationTime);
+            } catch (Exception) {
+                throw new BadRequestHttpException(sprintf("Unable to parse notification time '%s' in entry %d", $notificationTime, $itemIndex));
+            }
+        }
+
+        $type = Sanitize::cleanInt($mealData["type"] ?? null);
+
+        /**
+         * @var $mealType MealType
+         */
+        $mealType = $mealTypeRepository->find($type);// TODO: Restrict to current space?
+        if ($mealType === null) {
+            throw new BadRequestHttpException(sprintf("Invalid meal type %d in entry %d", $type, $itemIndex));
+        }
+
+        if ($id === null) {
+            $meal = new Meal;
+            $notification = null;
+        } else {
+            $meal = $mealRepository->find($id);// TODO: Restrict to current space?
+
+            if ($meal === null) {
+                throw new BadRequestHttpException(sprintf("Meal entry with ID %d does not exist", $id));
+            }
+
+            $notification = $meal->getNotification();
+        }
+
+        try {
+            $meal->setDate(new Date($date));
+        } catch (Exception) {
+            throw new BadRequestHttpException(sprintf("Unable to parse date '%s' in entry %d", $date, $itemIndex));
+        }
+
+        $meal->setSpace($space);
+        $meal->setType($mealType);
+        $meal->setText($text);
+        $meal->setUrl($url);
+
+        $entityManager->persist($meal);
+
+        if ($notificationDateTime === null) {
+            if ($notification !== null) {
+                $entityManager->remove($notification);
+            }
+        } else {
+            if ($notification === null) {
+                $notification = new Notification;
+                $notification->setMeal($meal);
+            }
+
+            $notification->setTime($notificationDateTime);
+            $notification->setText($notificationText);
+            $notification->setTriggered(false);
+            $entityManager->persist($notification);
+        }
+
+        return $meal;
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    private function triggerSaveWebhook(Config $config, Space $space, ?Date $savedWeek): void
+    {
+        $saveConfig = $config->get("app.save") ?? [];
+        $webhookUrl = $saveConfig["webhook-url"] ?? null;
+
+        if ($webhookUrl === null) {
+            return;
+        }
+
+        $client = new Client;
+        $client->post($webhookUrl, [
+            RequestOptions::TIMEOUT => $saveConfig["webhook-timeout"] ?? 0,
+            RequestOptions::JSON => [
+                "space" => $space->getId(),
+                "week" => $savedWeek?->formatForKey()
+            ]
+        ]);
     }
 }
