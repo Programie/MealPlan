39d50648f5585436c497ff091753b8b98fb726b9
Michael Wieland <git@selfcoders.com>
2023-04-22 19:41:45 +0200

    Fixed dropdown submenu on touch devices

--- src/main/resources/assets/script/dropdown-submenu.ts
+++ src/main/resources/assets/script/dropdown-submenu.ts
@@ -1,31 +1,53 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // make it as accordion for smaller screens
-    if (window.innerWidth < 992) {
-        // close all inner dropdowns when parent is closed
+class TouchDropdownSubmenu {
+    constructor() {
         document.querySelectorAll(".navbar .dropdown").forEach((dropdownElement) => {
-            dropdownElement.addEventListener("hidden.bs.dropdown", function () {
+            dropdownElement.addEventListener("hidden.bs.dropdown", () => {
                 // after dropdown is hidden, then find all submenus
-                this.querySelectorAll(".submenu").forEach((dropdownSubmenuElement: HTMLElement) => {
+                dropdownElement.querySelectorAll(".submenu").forEach((dropdownSubmenuElement: HTMLElement) => {
                     // hide every submenu as well
-                    dropdownSubmenuElement.style.display = "none";
+                    dropdownSubmenuElement.style.display = "";
                 });
             });
         });
 
-        document.querySelectorAll(".dropdown-menu a").forEach((dropdownMenuElement) => {
-            dropdownMenuElement.addEventListener("click", (event) => {
-                let nextElement = (event.target as HTMLElement).nextElementSibling as HTMLElement;
-                if (nextElement && nextElement.classList.contains("submenu")) {
-                    // prevent opening link if link needs to open dropdown
-                    event.preventDefault();
-
-                    if (nextElement.style.display == "block") {
-                        nextElement.style.display = "none";
-                    } else {
-                        nextElement.style.display = "block";
-                    }
-                }
-            });
-        })
+        document.querySelectorAll(".dropdown-menu button").forEach((dropdownMenuElement) => {
+            dropdownMenuElement.addEventListener("click", this.handleDropdownMenuEvent.bind(this));
+        });
     }
+
+    handleDropdownMenuEvent(event: Event) {
+        event.stopPropagation();
+
+        let dropdownMenuElement = event.target as HTMLElement;
+        let thisSubmenu = dropdownMenuElement.nextElementSibling;
+
+        let submenus = dropdownMenuElement.closest(".dropdown-menu").querySelectorAll(":scope > li > .submenu");
+        submenus.forEach((submenu: HTMLElement) => {
+            if (submenu === thisSubmenu) {
+                return;
+            }
+
+            submenu.style.display = "";
+        });
+
+        let nextElement = (event.target as HTMLElement).nextElementSibling as HTMLElement;
+        if (nextElement && nextElement.classList.contains("submenu")) {
+            // prevent opening link if link needs to open dropdown
+            event.preventDefault();
+
+            if (nextElement.style.display == "block") {
+                nextElement.style.display = "";
+            } else {
+                nextElement.style.display = "block";
+            }
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (navigator.maxTouchPoints === 0) {
+        return;
+    }
+
+    new TouchDropdownSubmenu();
 });
